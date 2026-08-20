cec9ba11f65c8c85f0270fe30b6005670d839fbb
ankurverma42-2026 <ankur_verma42@outlook.com>
2026-08-20 13:12:16 -0400

    Using Ollama instead of openAI

--- ragusingLangchain/main.py
+++ ragusingLangchain/main.py
@@ -1,14 +1,33 @@
 import os
-
 from dotenv import load_dotenv
-from langchain_ollama import OllamaLLM
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 def main():
-    llm = OllamaLLM(model="llama3.2")
-    result = llm.invoke("What is RAG")
+    llm = ChatOllama(model="llama3.2", temperature=0.1)
+
+    #prompt = ChatPromptTemplate.from_template("Answer precisely. Langchain is ...")
+    question="What is RAG?"
+    prompt=ChatPromptTemplate.from_messages(
+        [
+            ("system","You are AI tutor. Respond in one sentence with precise information."),
+            ("human",
+             "Here are some examples: \n\n"
+             "Question: What is RAG?"
+             "Response: Good  question! RAG stands for Retrieval Augmented Generation"
+             "Answer the questions starting with something like this:  "
+             "Nice one or something that motive user to ask more questions"
+             "Concept:\n\n {question}")
+        ])
+    # in lang chain, we wired from left instruction to right as below.
+    chain=prompt | llm
+  #  message = prompt.format_messages()
+    #chain.invoke does everything including formatting prompt and then calling llm.
+    result = chain.invoke({"question":question}).content
 
     print(result)
 
